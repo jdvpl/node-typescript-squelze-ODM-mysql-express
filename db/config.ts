376bd45da8e0dc0257a4cd9b5cea5f70498e5f61
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-04-29 22:22:13 -0500

    config de db y ssl para la base de datos en la nube

--- db/config.ts
+++ db/config.ts
@@ -1,8 +1,17 @@
 import {Sequelize} from 'sequelize';
+import dotenv from 'dotenv'
+// /config de env
+dotenv.config();
 
+console.log(process.env.DB)
 const db= new Sequelize(process.env.DB || 'node-tsc',process.env.DB_USER||'root',process.env.DB_PASSWORD ||'kakaroto',{
   host: process.env.DB_HOST||'localhost',
-  dialect:'mysql'
+  dialect:'mysql',
+  dialectOptions: {
+    ssl: {
+        rejectUnauthorized: true,        
+    }
+}
     // logging:false
 })
 
--- sql/sql.sql
+++ sql/sql.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `users` (
+CREATE TABLE `Users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
